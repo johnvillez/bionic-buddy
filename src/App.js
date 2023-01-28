d1e3cb80e8d4ea5bee47168e63b362d88c1b4512
John Villez <jmdvillez@gmail.com>
2023-01-28 15:52:46 -0800

    Added json placeholder to state. added loading text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -9,9 +8,15 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+    
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -22,13 +27,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>Bionic Buddies</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>        
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>LOADING . . .</h1>
+        } else {
+
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Bionic Buddies</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots}/>
+                </div>        
+            );
+        }
     }
 }
 
